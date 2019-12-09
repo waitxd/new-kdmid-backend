995ed07e0cc9b3bccbbace39c52b2c165343888f
AngularMaster <>
2019-12-09 15:00:30 -0300

    Image Zoom out with iso photo

--- server/automation/DS160/step_8_photo.js
+++ server/automation/DS160/step_8_photo.js
@@ -2,16 +2,32 @@ const mycore = require('../common')
 const path = require('path');
 const process = require('process')
 const aws_s3 = require('../../aws/s3')
+const fs = require('fs')
 
 const BTN_SAVE = '#container > button.button.button--success.button--save-photo'
 const BTN_NEXT = '#Wizard > div > div.wizard__body > div.step__action-area > nav > div > button.pagination__button.pagination__button--next.button'
+const BTN_ZOOM_OUT = 'container > button.button.button--primary.button--photo-action.button--zoom-out'
+const ZOOM_OUT_COUNT = 20
 
-withPhoto = async (page, url) => {
+withPhoto = async (page, url, base64) => {
     const fileInput = await page.$('input[type=file]');
     const filePath = path.relative(process.cwd(), path.join(__dirname + '/images/customer.jpeg'))
     const prefix = 'https://s3.us-east-2.amazonaws.com/assets.new-kdmid/'
-    const result = await aws_s3.downloadFile(filePath, url.substring(prefix.length))
+    // const result = await aws_s3.downloadFile(filePath, url.substring(prefix.length))
+
+    let base64Data = base64.replace(/^data:image\/jpeg;base64,/,"")
+    fs.writeFile(filePath, base64Data, 'base64', function(err) {
+        console.log(err)
+    })
+
     await fileInput.uploadFile(filePath);
+
+    // zoom out image start
+
+    for(let i = 0; i < ZOOM_OUT_COUNT; i++)
+        await mycore.Auto_Button(page, BTN_ZOOM_OUT)
+
+    // zoom out image finish
 
     await page.waitForFunction(`document.querySelector("${BTN_SAVE}") && document.querySelector("${BTN_SAVE}").getAttribute("disabled") == null`)
     await page.evaluate(`document.querySelector("${BTN_SAVE}").focus();document.querySelector("${BTN_SAVE}").click();`)
@@ -24,6 +40,7 @@ withPhoto = async (page, url) => {
         });
         console.log('failed with uploaded photo. The customer should prepare a photo again.', errors)
         // await withoutPhoto(page, url)
+        throw "Failed to upload photo"
         return;
     }
     console.log('Success with this photo')
@@ -34,6 +51,6 @@ withPhoto = async (page, url) => {
 processStep = async (page, data) => {
     const { photo } = data
     await mycore.Auto_Button(page, 'btnApplicantPhotoModal', true)
-    await withPhoto(page, photo.url)
+    await withPhoto(page, photo.url, photo.iso)
 }
 exports.process = processStep
